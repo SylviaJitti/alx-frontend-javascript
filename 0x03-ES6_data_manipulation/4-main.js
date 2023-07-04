b9f10fc29a5c6d7b9c3758e8f1ae59c73051733c
root <SylviaJitti>
2023-07-04 14:19:11 +0000

    typed arrays

--- 0x03-ES6_data_manipulation/4-main.js
+++ 0x03-ES6_data_manipulation/4-main.js
@@ -1,10 +1,7 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-      const grade = gradeObj ? gradeObj.grade : 'N/A';
+import getListStudents from "./0-get_list_students.js";
+import updateStudentGradeByCity from "./4-update_grade_by_city.js";
 
-      return { ...student, grade };
-    });
-}
+console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }, { studentId: 1, grade: 86 }]));
+
+console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }]));
+
