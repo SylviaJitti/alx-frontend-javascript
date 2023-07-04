0aca7f308f1f60e9c34e685d03ff72b55b6151d2
root <SylviaJitti>
2023-07-04 14:57:30 +0000

    values similar in set and arrays

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,3 @@
-export default function cleanSet(set, startString) {
-  if (!startString || !set || startString.length === 0) {
-    return '';
-  }
-  return Array.from(set).filter((item) => item.startsWith(startString)).map((item) => item.slice(startString.length)).join('-');
+export default function hasValuesFromArray(set, arr) {
+  return arr.every((element) => set.has(element));
 }
