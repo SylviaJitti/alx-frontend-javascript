cd997c0b37c26d590923a307180e85742a03dccf
SylviaJitti <sylviajitti@gmail.com>
2023-07-07 14:06:17 +0100

    task 3 Ambient Namespaces

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,85 @@
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+  }
+  
+  interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+  }
+  
+  class Director implements DirectorInterface {
+    workFromHome(): string {
+      return "Working from home";
+    }
+  
+    getCoffeeBreak(): string {
+      return "Getting a coffee break";
+    }
+  
+    workDirectorTasks(): string {
+      return "Getting to director tasks";
+    }
+  }
+  
+  class Teacher implements TeacherInterface {
+    workFromHome(): string {
+      return "Cannot work from home";
+    }
+  
+    getCoffeeBreak(): string {
+      return "Cannot have a break";
+    }
+  
+    workTeacherTasks(): string {
+      return "Getting to work";
+    }
+  }
+  
+  function createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === "number" && salary < 500) {
+      return new Teacher();
+    } else {
+      return new Director();
+    }
+  }
+//functions isdirector and excecutework
+
+  function isDirector(employee: Director | Teacher): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+  }
+  
+  function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+      return employee.workDirectorTasks();
+    } else {
+      return employee.workTeacherTasks();
+    }
+  }
+  //implementation 
+  const director = new Director();
+  const teacher = new Teacher();
+  
+  console.log(executeWork(director));
+  console.log(executeWork(teacher)); 
+
+  // string literal "subjects"
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  } else if (todayClass === "History") {
+    return "Teaching History";
+  } else {
+    return "Unknown subject";
+  }
+}
+
+// Example implementation:
+console.log(teachClass("Math")); 
+console.log(teachClass("History")); 
+
